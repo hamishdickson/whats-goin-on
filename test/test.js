3eae6b60d539b3bafb5296e51064655f8634da62
mishyTheBear <hamish.dickson@gmail.com>
2015-02-08 19:01:13 +0000

    new text (woooo)

--- test/test.js
+++ test/test.js
@@ -2,8 +2,9 @@ var test = require('tape')
 var exec = require('child_process').exec
 
 test('verify hamishdickson returns stuff', function(t) {
-  exec('node index.js hamishdickson', function(error, stdout, stderr) {
-    t.notOk(error, "no error running command")
-  })
-  t.end()
-})
+    exec('node index.js hamishdickson', function(error, stdout, stderr) {
+        t.notOk(error, "no error running command");
+        t.notEqual(stdout, "", "body not empty");
+    });
+    t.end();
+});
